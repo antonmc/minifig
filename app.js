0fbb92f5ab1de77d9f2840188d561eab96477354
Anton McConville <anton@hickory.ca>
2018-04-15 11:44:16 -0400

    more randomness

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ var appEnv = cfenv.getAppEnv();
 
 var folder = "frankenstein";
 
+var skincolors = [ "#f9ac2f", "#a67c52" ];
+var haircolors = [ "#754c24", "#c7b299", "#a54632", "#42210b"];
+var sweatercolors = ["#f7cee0","#c3dbd4","#b61c50","#6c4e79", "#223a5e","#64bfa4", "#fe840e"];
+
 var body = "/body";
 var hair = "/hair";
 var face = "/face";
@@ -24,12 +28,16 @@ var face = "/face";
 var prefix = folder + body + "/prefix.svg";
 var postfix = folder + body + "/postfix.svg";
 var style = folder + body + "/style.svg";
-var hair = folder + hair + "/hair1.svg"
+var hair = folder + hair + "/hair" + getRandomInt(3) + ".svg"
 var base = folder + body + "/base.svg"
 
 // write the style file
 
-var styleblock = createStyle("#a67c52","#c7b299","#c3dbd4")
+var randomHair = haircolors[getRandomInt(haircolors.length)]
+var randomSkin = skincolors[getRandomInt(skincolors.length)]
+var randomSweater = sweatercolors[getRandomInt(sweatercolors.length)]
+
+var styleblock = createStyle(randomSkin, randomHair, randomSweater)
 
 fs.writeFile(style, styleblock, function(err) {
     if(err) {
@@ -54,6 +62,13 @@ fs.writeFile(style, styleblock, function(err) {
     console.log('done');
   });
 
+
+
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
   function createStyle(skincolor, haircolor, sweatercolor){
 
     var open = '<style type="text/css">\n'
@@ -61,7 +76,7 @@ fs.writeFile(style, styleblock, function(err) {
 
     var skin = '.skin{fill:' + skincolor + ';stroke:#000000;stroke-width:2;stroke-linecap:round;stroke-miterlimit:10;}\n';
     var sweater = '.sweater{fill:' + sweatercolor + ';stroke:#000000;stroke-width:2;stroke-linecap:round;stroke-miterlimit:10;}\n';
-    var hair = '.hair{fill:#42210B;}\n'
+    var hair = '.hair{fill:' + haircolor + ';}\n'
 
     var close = '</style>'
 
@@ -72,7 +87,7 @@ fs.writeFile(style, styleblock, function(err) {
 
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-});
+// app.listen(appEnv.port, '0.0.0.0', function() {
+//   // print a message when the server starts listening
+//   console.log("server starting on " + appEnv.url);
+// });
